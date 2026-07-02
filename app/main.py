5c4f9565b2698d97e36e17341a0d4d2469f0faee
RaimeiRaikiri <mikescutts10@gmail.com>
2026-07-03 00:01:41 +0100

    feat(app): connected routers to app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.models import Base
 from app.database import engine
+from app.routers import auth, users, applications
 
 app = FastAPI(
     title="Job application tracker",
@@ -11,3 +12,7 @@ app = FastAPI(
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
+    
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(applications.router)
